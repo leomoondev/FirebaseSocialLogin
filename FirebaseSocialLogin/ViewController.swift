4359b04ebd5214081550c6cf641482e5dba9ca62
Leo Moon <leomoondev@gmail.com>
2017-02-03 13:43:49 -0800

    Updated ViewController and AppDelegate

---
+++ FirebaseSocialLogin/ViewController.swift
@@ -0,0 +1,94 @@
+//
+//  ViewController.swift
+//  FirebaseSocialLogin
+//
+//  Created by Hyung Jip Moon on 2017-02-02.
+//  Copyright © 2017 leomoon. All rights reserved.
+//
+
+import UIKit
+import FBSDKLoginKit
+import Firebase
+
+class ViewController: UIViewController, FBSDKLoginButtonDelegate {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let loginButton = FBSDKLoginButton()
+        
+        view.addSubview(loginButton)
+        loginButton.frame = CGRect(x: 16, y:50, width: view.frame.width - 32, height: 50)
+        
+        loginButton.delegate = self
+        loginButton.readPermissions = ["email", "public_profile"]
+        
+        // Add the custom Facebook login button here
+        let customFBButton = UIButton()
+        customFBButton.backgroundColor = .blue
+        customFBButton.frame = CGRect(x: 16, y: 116, width: view.frame.width - 32, height: 50)
+        customFBButton.setTitle("Custom Facebook Login Here", for: .normal)
+        customFBButton.titleLabel?.font = UIFont.boldSystemFont(ofSize: 16)
+        view.addSubview(customFBButton)
+        
+        customFBButton.addTarget(self, action: #selector(handleCustomFBLogin), for: .touchUpInside)
+    }
+    
+    func handleCustomFBLogin() {
+        FBSDKLoginManager().logIn(withReadPermissions: ["email", "public_profile"], from: self)
+        { (result, err) in
+            if err != nil {
+                print("Custom Facebook Login failed:", err!)
+                return
+            }
+            print(result?.token.tokenString as Any)
+        }
+        
+        self.showEmailAddress()
+        
+    }
+    
+    func loginButtonDidLogOut(_ loginButton: FBSDKLoginButton!) {
+        print("Did log out of facebook")
+        
+    }
+    
+    func loginButton(_ loginButton: FBSDKLoginButton!, didCompleteWith result: FBSDKLoginManagerLoginResult!, error:Error!) {
+        
+        if error != nil {
+            print(error)
+            return
+        }
+
+        showEmailAddress()
+        
+    }
+    
+    func showEmailAddress() {
+        let accessToken = FBSDKAccessToken.current()
+        guard let accessTokenString = accessToken?.tokenString else
+        { return }
+        
+        let credentials =
+        FIRFacebookAuthProvider.credential(withAccessToken: accessTokenString)
+        FIRAuth.auth()?.signIn(with: credentials, completion: {(user, error) in
+            if error != nil {
+                print("Something went wrong with our Facebook user: ", error ?? "")
+                return
+            }
+            print("Successfully logged in with our user: ", user ?? "")
+        })
+
+        FBSDKGraphRequest(graphPath: "/me", parameters: ["fields": "id, name, email"]).start {
+            (connection, result, err) in
+            
+            if err != nil {
+                print("Failed to start graph request:", err!)
+                return
+            }
+            print(result!)
+        }
+    }
+
+}
+
