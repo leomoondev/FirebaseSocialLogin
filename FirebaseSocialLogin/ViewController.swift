31d6b20cd3ae3b289844784cc5e589e480009693
Leo Moon <leomoondev@gmail.com>
2017-02-03 16:08:31 -0800

    Added google sign-in function

--- FirebaseSocialLogin/ViewController.swift
+++ FirebaseSocialLogin/ViewController.swift
@@ -9,86 +9,112 @@
 import UIKit
 import FBSDKLoginKit
 import Firebase
+import GoogleSignIn
 
-class ViewController: UIViewController, FBSDKLoginButtonDelegate {
+class ViewController: UIViewController, FBSDKLoginButtonDelegate, GIDSignInUIDelegate {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        let loginButton = FBSDKLoginButton()
+        setupFacebookButtons()
         
+        setupGoogleButtons()
+    }
+    
+    fileprivate func setupGoogleButtons() {
+        
+        // Add google sign-in button
+        let googleButton = GIDSignInButton()
+        googleButton.frame = CGRect(x: 16, y: 116 + 66, width: view.frame.width - 32, height: 50)
+        view.addSubview(googleButton)
+        
+        let customButton = UIButton(type: .system)
+        customButton.frame = CGRect(x: 16, y: 116 + 66 + 66, width: view.frame.width - 32, height: 50)
+        customButton.backgroundColor = .orange
+        customButton.setTitle("Custom Google Sign In", for: .normal)
+        customButton.addTarget(self, action: #selector(handleCustomGoogleSign), for: .touchUpInside)
+        customButton.setTitleColor(.white, for: .normal)
+        customButton.titleLabel?.font = UIFont.boldSystemFont(ofSize: 14)
+        view.addSubview(customButton)
+        
+        GIDSignIn.sharedInstance().uiDelegate = self
+    }
+    
+    func handleCustomGoogleSign() {
+        GIDSignIn.sharedInstance().signIn()
+    }
+    
+    fileprivate func setupFacebookButtons() {
+        let loginButton = FBSDKLoginButton()
         view.addSubview(loginButton)
-        loginButton.frame = CGRect(x: 16, y:50, width: view.frame.width - 32, height: 50)
+        //frame's are obselete, please use constraints instead because its 2016 after all
+        loginButton.frame = CGRect(x: 16, y: 50, width: view.frame.width - 32, height: 50)
         
         loginButton.delegate = self
         loginButton.readPermissions = ["email", "public_profile"]
         
         // Add the custom Facebook login button here
-        let customFBButton = UIButton()
+        let customFBButton = UIButton(type: .system)
         customFBButton.backgroundColor = .blue
         customFBButton.frame = CGRect(x: 16, y: 116, width: view.frame.width - 32, height: 50)
-        customFBButton.setTitle("Custom Facebook Login Here", for: .normal)
+        customFBButton.setTitle("Custom FB Login here", for: .normal)
         customFBButton.titleLabel?.font = UIFont.boldSystemFont(ofSize: 16)
+        customFBButton.setTitleColor(.white, for: .normal)
         view.addSubview(customFBButton)
         
         customFBButton.addTarget(self, action: #selector(handleCustomFBLogin), for: .touchUpInside)
     }
     
     func handleCustomFBLogin() {
-        FBSDKLoginManager().logIn(withReadPermissions: ["email", "public_profile"], from: self)
-        { (result, err) in
+        FBSDKLoginManager().logIn(withReadPermissions: ["email"], from: self) { (result, err) in
             if err != nil {
-                print("Custom Facebook Login failed:", err!)
+                print("Custom FB Login failed:", err!)
                 return
             }
-            print(result?.token.tokenString as Any)
+            
+            self.showEmailAddress()
         }
-        
-        self.showEmailAddress()
-        
+    }
+
+
+
+
+    
+func loginButtonDidLogOut(_ loginButton: FBSDKLoginButton!) {
+    print("Did log out of facebook")
+}
+
+func loginButton(_ loginButton: FBSDKLoginButton!, didCompleteWith result: FBSDKLoginManagerLoginResult!, error: Error!) {
+    if error != nil {
+        print(error)
+        return
     }
     
-    func loginButtonDidLogOut(_ loginButton: FBSDKLoginButton!) {
-        print("Did log out of facebook")
-        
-    }
+    showEmailAddress()
+}
+
+func showEmailAddress() {
+    let accessToken = FBSDKAccessToken.current()
+    guard let accessTokenString = accessToken?.tokenString else { return }
     
-    func loginButton(_ loginButton: FBSDKLoginButton!, didCompleteWith result: FBSDKLoginManagerLoginResult!, error:Error!) {
-        
+    let credentials = FIRFacebookAuthProvider.credential(withAccessToken: accessTokenString)
+    FIRAuth.auth()?.signIn(with: credentials, completion: { (user, error) in
         if error != nil {
-            print(error)
+            print("Something went wrong with our FB user: ", error ?? "")
             return
         }
-
-        showEmailAddress()
         
-    }
-    
-    func showEmailAddress() {
-        let accessToken = FBSDKAccessToken.current()
-        guard let accessTokenString = accessToken?.tokenString else
-        { return }
-        
-        let credentials =
-        FIRFacebookAuthProvider.credential(withAccessToken: accessTokenString)
-        FIRAuth.auth()?.signIn(with: credentials, completion: {(user, error) in
-            if error != nil {
-                print("Something went wrong with our Facebook user: ", error ?? "")
-                return
-            }
-            print("Successfully logged in with our user: ", user ?? "")
-        })
+        print("Successfully logged in with our user: ", user ?? "")
+    })
 
-        FBSDKGraphRequest(graphPath: "/me", parameters: ["fields": "id, name, email"]).start {
-            (connection, result, err) in
+        FBSDKGraphRequest(graphPath: "/me", parameters: ["fields": "id, name, email"]).start { (connection, result, err) in
             
             if err != nil {
-                print("Failed to start graph request:", err!)
+                print("Failed to start graph request:", err ?? "")
                 return
             }
-            print(result!)
+            print(result ?? "")
         }
-    }
-
+}
 }
 
